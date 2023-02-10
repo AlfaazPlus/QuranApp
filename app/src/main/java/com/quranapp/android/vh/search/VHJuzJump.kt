704802b91a570ea41d2f35a32035aac754493240
faisalcodes <faisalknx@gmail.com>
2023-02-10 14:23:50 +0530

    Part of Kotlin Migration.
    Co-authored-by: Ankush Verma <ankushv20@yahoo.com>

--- app/src/main/java/com/quranapp/android/vh/search/VHJuzJump.kt
+++ app/src/main/java/com/quranapp/android/vh/search/VHJuzJump.kt
@@ -1,26 +1,21 @@
-package com.quranapp.android.vh.search;
+package com.quranapp.android.vh.search
 
-import com.quranapp.android.components.search.JuzJumpModel;
-import com.quranapp.android.components.search.SearchResultModelBase;
-import com.quranapp.android.databinding.LytReaderJuzSpinnerItemBinding;
-import com.quranapp.android.utils.reader.factory.ReaderFactory;
+import com.quranapp.android.components.search.JuzJumpModel
+import com.quranapp.android.components.search.SearchResultModelBase
+import com.quranapp.android.databinding.LytReaderJuzSpinnerItemBinding
+import com.quranapp.android.utils.reader.factory.ReaderFactory.startJuz
 
-public class VHJuzJump extends VHSearchResultBase {
-    private final LytReaderJuzSpinnerItemBinding mBinding;
-
-    public VHJuzJump(LytReaderJuzSpinnerItemBinding binding, boolean applyMargins) {
-        super(binding.getRoot());
-        mBinding = binding;
-
-        setupJumperView(binding.getRoot(), applyMargins);
+class VHJuzJump(private val mBinding: LytReaderJuzSpinnerItemBinding, applyMargins: Boolean) : VHSearchResultBase(
+    mBinding.root
+) {
+    init {
+        setupJumperView(mBinding.root, applyMargins)
     }
 
-    @Override
-    public void bind(SearchResultModelBase parentModel, int pos) {
-        JuzJumpModel model = (JuzJumpModel) parentModel;
-
-        mBinding.juzSerial.setText(model.juzSerial);
-
-        mBinding.getRoot().setOnClickListener(v -> ReaderFactory.startJuz(v.getContext(), model.juzNo));
+    override fun bind(model: SearchResultModelBase, pos: Int) {
+        (model as JuzJumpModel).apply {
+            mBinding.juzSerial.text = model.juzSerial
+            mBinding.root.setOnClickListener { startJuz(it.context, model.juzNo) }
+        }
     }
 }
--- app/src/main/java/com/quranapp/android/vh/search/VHSearchResultBase.kt
+++ app/src/main/java/com/quranapp/android/vh/search/VHSearchResultBase.kt
@@ -1,35 +1,24 @@
-package com.quranapp.android.vh.search;
+package com.quranapp.android.vh.search
 
-import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
-import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
+import android.view.View
+import android.view.ViewGroup
+import android.view.ViewGroup.MarginLayoutParams
+import androidx.core.view.updateMarginsRelative
+import androidx.recyclerview.widget.RecyclerView
+import com.peacedesign.android.utils.Dimen
+import com.quranapp.android.R
+import com.quranapp.android.components.search.SearchResultModelBase
 
-import android.view.View;
-import android.view.ViewGroup;
+open class VHSearchResultBase(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    open fun bind(model: SearchResultModelBase, pos: Int) {}
 
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
-
-import com.peacedesign.android.utils.Dimen;
-import com.peacedesign.android.utils.ViewUtils;
-import com.quranapp.android.R;
-import com.quranapp.android.components.search.SearchResultModelBase;
-
-public class VHSearchResultBase extends RecyclerView.ViewHolder {
-    public VHSearchResultBase(@NonNull View itemView) {
-        super(itemView);
-    }
-
-    public void bind(SearchResultModelBase model, int pos) {
-    }
-
-    protected void setupJumperView(View view, boolean applyMargins) {
-        ViewGroup.MarginLayoutParams params = new ViewGroup.MarginLayoutParams(MATCH_PARENT, WRAP_CONTENT);
-        if (applyMargins) {
-            ViewUtils.setMarginHorizontal(params, Dimen.dp2px(view.getContext(), 10));
-            params.bottomMargin = Dimen.dp2px(view.getContext(), 10);
+    protected fun setupJumperView(view: View, applyMargins: Boolean) {
+        view.layoutParams = MarginLayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT).apply {
+            if (applyMargins) {
+                val marg = Dimen.dp2px(view.context, 10f)
+                updateMarginsRelative(start = marg, end = marg, bottom = 10)
+            }
         }
-
-        view.setLayoutParams(params);
-        view.setBackgroundResource(R.drawable.dr_bg_chapter_card_bordered_onlylight);
+        view.setBackgroundResource(R.drawable.dr_bg_chapter_card_bordered_onlylight)
     }
 }
