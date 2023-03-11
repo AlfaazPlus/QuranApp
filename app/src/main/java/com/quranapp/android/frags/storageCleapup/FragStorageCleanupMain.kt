3ec10f2f607577224288c290366665dc3784045d
faisalcodes <faisalknx@gmail.com>
2023-03-11 16:32:30 +0530

    Work on storage cleanup ui

--- app/src/main/java/com/quranapp/android/frags/storageCleapup/FragStorageCleanupMain.kt
+++ app/src/main/java/com/quranapp/android/frags/storageCleapup/FragStorageCleanupMain.kt
@@ -14,6 +14,7 @@ import com.quranapp.android.utils.univ.FileUtils
 
 class FragStorageCleanupMain : FragStorageCleanupBase() {
     private lateinit var fileUtils: FileUtils
+    private lateinit var binding: FragStorageCleanupMainBinding
 
     override fun getFragTitle(ctx: Context) = ctx.getString(R.string.titleStorageCleanup)
 
@@ -23,9 +24,14 @@ class FragStorageCleanupMain : FragStorageCleanupBase() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         fileUtils = FileUtils.newInstance(view.context)
-        init(FragStorageCleanupMainBinding.bind(view))
+        binding = FragStorageCleanupMainBinding.bind(view)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        init(binding)
     }
 
     private fun init(binding: FragStorageCleanupMainBinding) {
