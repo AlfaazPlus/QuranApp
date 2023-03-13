fb84eef1ebdd88da66ae229ea8b28d4de0e99fb3
faisalcodes <faisalknx@gmail.com>
2023-03-13 14:03:51 +0530

    Fixed: App crashing when opened without alarm permission granted.

---
+++ app/src/main/java/com/quranapp/android/utils/receivers/AlarmPermissionReceiver.kt
@@ -0,0 +1,15 @@
+package com.quranapp.android.utils.receivers
+
+import android.app.AlarmManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import com.quranapp.android.utils.votd.VOTDUtils
+
+class AlarmPermissionReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent?) {
+        if (AlarmManager.ACTION_SCHEDULE_EXACT_ALARM_PERMISSION_STATE_CHANGED == intent?.action && VOTDUtils.isVOTDTrulyEnabled(context)) {
+            VOTDUtils.enableVOTDReminder(context)
+        }
+    }
+}
