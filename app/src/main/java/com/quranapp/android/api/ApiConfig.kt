272564266cc1aa4d337b959fce9e403465a871ad
Faisal Khan <faisalknx@gmail.com>
2023-04-27 17:20:03 +0530

    Minor Fixes in ApiConfig.kt

--- app/src/main/java/com/quranapp/android/api/ApiConfig.kt
+++ app/src/main/java/com/quranapp/android/api/ApiConfig.kt
@@ -7,7 +7,7 @@
 package com.quranapp.android.api
 
 object ApiConfig {
-    const val GITHUB_ROOT_URL = "https://cdn.jsdelivr.net/gh/AlfaazPlus/QuranApp/"
+    const val GITHUB_ROOT_URL = "https://cdn.jsdelivr.net/gh/AlfaazPlus/QuranApp@latest/"
     const val GITHUB_REPOSITORY_URL = "https://github.com/AlfaazPlus/QuranApp"
     const val GITHUB_ISSUES_BUG_REPORT_URL = "https://github.com/AlfaazPlus/QuranApp/issues/new?template=bug_report.yml"
     const val GITHUB_ISSUES_VERSE_REPORT_URL = "https://github.com/AlfaazPlus/QuranApp/issues/new?template=verse_report.yml"
