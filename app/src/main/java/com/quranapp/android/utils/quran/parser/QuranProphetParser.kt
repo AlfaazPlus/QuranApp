56ea496e58862fbb3997b79689ab1daf1c0ebb9e
faisalcodes <faisalknx@gmail.com>
2023-08-13 12:30:07 +0530

    Organized Quranic prophets files

--- app/src/main/java/com/quranapp/android/utils/quran/parser/QuranProphetParser.kt
+++ app/src/main/java/com/quranapp/android/utils/quran/parser/QuranProphetParser.kt
@@ -1,13 +1,10 @@
 package com.quranapp.android.utils.quran.parser
 
 import android.content.Context
-import android.content.res.XmlResourceParser
-import android.util.SparseArray
 import com.quranapp.android.R
 import com.quranapp.android.components.quran.QuranMeta
 import com.quranapp.android.components.quran.QuranProphet
 import com.quranapp.android.components.quran.QuranProphet.Prophet
-import com.quranapp.android.components.quran.QuranProphet.ProphetReference
 import com.quranapp.android.utils.Log
 import com.quranapp.android.utils.quran.parser.ParserUtils.prepareChapterText
 import com.quranapp.android.utils.quran.parser.ParserUtils.prepareChaptersList
@@ -27,16 +24,19 @@ object QuranProphetParser {
     private const val PROPHETS_ATTR_NAME_AR = "name-ar"
     private const val PROPHETS_ATTR_NAME_EN = "name-en"
     private const val PROPHETS_ATTR_NAME = "name"
-    private const val PROPHETS_ATTR_HONORIFIC = "honorific"
     private const val PROPHETS_ATTR_ICON_RES = "drawable"
 
-    fun parseProphet(context: Context, quranMeta: QuranMeta, instanceRef: AtomicReference<QuranProphet>, callback: () -> Unit) {
+    fun parseProphet(
+        context: Context,
+        quranMeta: QuranMeta,
+        instanceRef: AtomicReference<QuranProphet>,
+        callback: () -> Unit
+    ) {
         CoroutineScope(Dispatchers.IO).launch {
             try {
-                val parsedQuranProphets = parseProphetInternal(
+                val parsedQuranProphets = parseProphetsInternal(
                     context,
                     quranMeta,
-                    parseProphetReferencesInternal(context.resources.getXml(R.xml.quran_prophets_reference))
                 )
 
                 instanceRef.set(parsedQuranProphets)
@@ -48,73 +48,61 @@ object QuranProphetParser {
         }
     }
 
-    private fun parseProphetReferencesInternal(parser: XmlResourceParser): SparseArray<ProphetReference> {
-        val referenceArray = SparseArray<ProphetReference>()
-        var lastReference: ProphetReference? = null
+    @Throws(XmlPullParserException::class, IOException::class)
+    private fun parseProphetsInternal(
+        context: Context,
+        quranMeta: QuranMeta,
+    ): QuranProphet {
+        val parser = context.resources.getXml(R.xml.quran_prophets_reference)
+        val prophetList: MutableList<Prophet> = ArrayList()
+
+        val honorificMuhammad = context.getString(R.string.honorificMuhammad)
+        val honorificProphet = context.getString(R.string.honorificProphet)
+
+        var lastReference: Prophet? = null
 
         while (parser.next() != XmlPullParser.END_DOCUMENT) {
             when (parser.eventType) {
                 XmlPullParser.START_TAG -> {
-                    if (PROPHETS_TAG_PROPHET.equals(parser.name, ignoreCase = true)) {
-                        lastReference = ProphetReference(
-                            order = parser.getAttributeIntValue(null, PROPHETS_ATTR_ORDER, -1),
-                            nameAr = parser.getAttributeValue(null, PROPHETS_ATTR_NAME_AR),
-                            nameEn = parser.getAttributeValue(null, PROPHETS_ATTR_NAME_EN),
-                            iconRes = parser.getAttributeResourceValue(
+                    if (!PROPHETS_TAG_PROPHET.equals(parser.name, ignoreCase = true)) {
+                        continue
+                    }
+
+                    val order = parser.getAttributeIntValue(null, PROPHETS_ATTR_ORDER, -1)
+                    val lastProphet = Prophet(
+                        order = order,
+                        nameAr = parser.getAttributeValue(null, PROPHETS_ATTR_NAME_AR),
+                        nameEn = parser.getAttributeValue(null, PROPHETS_ATTR_NAME_EN),
+                        name = context.getString(
+                            parser.getAttributeResourceValue(
                                 "http://schemas.android.com/apk/res/android",
-                                PROPHETS_ATTR_ICON_RES,
+                                PROPHETS_ATTR_NAME,
                                 -1
                             )
-                        )
+                        ),
+                        honorific = if (order == 25) honorificMuhammad else honorificProphet,
+                        iconRes = parser.getAttributeResourceValue(
+                            "http://schemas.android.com/apk/res/android",
+                            PROPHETS_ATTR_ICON_RES,
+                            -1
+                        ),
+                    )
 
-                        referenceArray.put(lastReference.order, lastReference)
-                    }
+                    lastReference = lastProphet
+                    prophetList.add(lastProphet)
                 }
+
                 XmlPullParser.TEXT -> {
-                    if (lastReference != null) {
-                        lastReference.references = parser.text
+                    lastReference?.let { prophet ->
+                        prophet.references = parser.text
+                        prophet.references?.let {
+                            prophet.verses = prepareVersesList(it, true)
+                            prophet.chapters = prepareChaptersList(prophet.verses)
+                            prophet.inChapters = prepareChapterText(context, quranMeta, prophet.chapters, 2)
+                        }
                     }
                 }
             }
-        }
-
-        return referenceArray
-    }
-
-    @Throws(XmlPullParserException::class, IOException::class)
-    private fun parseProphetInternal(
-        context: Context,
-        quranMeta: QuranMeta,
-        prophetReferences: SparseArray<ProphetReference>
-    ): QuranProphet {
-        val parser = context.resources.getXml(R.xml.quran_prophets)
-        val prophetList: MutableList<Prophet> = ArrayList()
-
-        while (parser.next() != XmlPullParser.END_DOCUMENT) {
-            if (parser.eventType != XmlPullParser.START_TAG || !PROPHETS_TAG_PROPHET.equals(parser.name, ignoreCase = true)) {
-                continue
-            }
-
-            val order = parser.getAttributeIntValue(null, PROPHETS_ATTR_ORDER, -1)
-            val reference = prophetReferences[order]
-
-            val lastProphet = Prophet(
-                order = order,
-                nameAr = reference.nameAr,
-                nameEn = reference.nameEn,
-                name = parser.getAttributeValue(null, PROPHETS_ATTR_NAME),
-                honorific = parser.getAttributeValue(null, PROPHETS_ATTR_HONORIFIC),
-                iconRes = reference.iconRes,
-            )
-
-            reference.references?.let {
-                lastProphet.verses = prepareVersesList(it, true)
-                lastProphet.chapters = prepareChaptersList(lastProphet.verses)
-                lastProphet.inChapters = prepareChapterText(context, quranMeta, lastProphet.chapters, 2)
-            }
-
-            prophetList.add(lastProphet)
-
         }
 
         prophetList.sortBy { it.order }
