0d4575b471d54644faf32d5c58f9156cdc26e813
arkibax <155833069+arkibax@users.noreply.github.com>
2024-03-12 19:53:52 +0100

    removed unused imports

--- app/src/main/java/com/quranapp/android/ui/screens/ReadHistoryScreen.kt
+++ app/src/main/java/com/quranapp/android/ui/screens/ReadHistoryScreen.kt
@@ -11,25 +11,15 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.DismissDirection
-import androidx.compose.material.DismissValue
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
-import androidx.compose.material.SwipeToDismiss
 import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.rememberDismissState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
@@ -105,7 +95,7 @@ fun ReadHistoryScreen() {
 }
 
 
-@OptIn(ExperimentalMaterialApi::class)
+
 @Composable
 fun ReadHistoryList(
     modifier: Modifier = Modifier,
